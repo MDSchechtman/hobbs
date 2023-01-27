ec0fd8d56a1ef2da677ca7a3b3bf239d1ebd7b4e
Matt Schechtman <matthew.d.schechtman@gmail.com>
2023-01-26 19:59:31 -0500

    add rgb value handling etc.

--- led_test/src/main.cpp
+++ led_test/src/main.cpp
@@ -7,6 +7,12 @@
 #include "FastLED.h"
 #include <Arduino.h>
 
+// my stuff
+int current_color = 0; // 0 - R, 1 - G, 2 - B
+int rgb[] = {0, 0, 0};
+boolean color_selected = false;
+//
+
 #define NUM_LEDS 10
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
@@ -58,30 +64,67 @@ void setup_display()
 }
 
 // Rotary Encoder
-#define ROTARY_PIN1	11
-#define ROTARY_PIN2	12
-#define BUTTON_PIN	13
-#define CLICKS_PER_STEP 4   
+#define ROTARY_PIN1 11
+#define ROTARY_PIN2 12
+#define BUTTON_PIN 13
+#define CLICKS_PER_STEP 4
 ESPRotary rotary;
 Button2 rotary_button;
 
-void rotate(ESPRotary& r) {
-   Serial.println(r.getPosition());
+void rotate(ESPRotary &r)
+{
+  if (!color_selected)
+  {
+    if (r.getDirection() == right)
+    {
+      current_color++;
+    }
+    else
+    {
+      current_color--;
+    }
+
+    // loop around
+    current_color = current_color % 3; 
+  }
+  else 
+  {
+    if (r.getDirection() == right)
+    {
+      rgb[current_color]++;
+    }
+    else
+    {
+      rgb[current_color]--;
+    }
+    
+    // loop around
+    rgb[current_color] == rgb[current_color] % 255;
+  }
+  Serial.println(r.getPosition());
 }
 
 // on left or right rotation
-void showDirection(ESPRotary& r) {
+void showDirection(ESPRotary &r)
+{
   Serial.println(r.directionToString(r.getDirection()));
 }
 
 // single click
-void click(Button2& btn) {
+void click(Button2 &btn)
+{
+  color_selected = !color_selected;
   Serial.println("Click!");
 }
 
 // long click
-void resetPosition(Button2& btn) {
+void resetPosition(Button2 &btn)
+{
   rotary.resetPosition();
+  color_selected = false;
+  rgb[0] = 0;
+  rgb[1] = 0;
+  rgb[2] = 0;
   Serial.println("Reset!");
 }
 
@@ -107,15 +150,93 @@ void setup()
   setup_encoder();
 }
 
+void draw_header()
+{
+  display.setCursor(0, 0);
+
+  String rgb_text0 = "(";
+  String rgb_text1 = rgb_text0 + rgb[0];
+  String rgb_text2 = rgb_text1 + ", ";
+  String rgb_text3 = rgb_text2 + rgb[1];
+  String rgb_text4 = rgb_text3 + ", ";
+  String rgb_text5 = rgb_text4 + rgb[2];
+  String rgb_text6 = rgb_text5 + ")";
+  display.println(rgb_text6);
+}
+
+void draw_body()
+{
+  display.setCursor(20, 0);
+
+  if (!color_selected)
+  {
+    if (current_color == 0)
+    {
+      display.print("> ");
+    }
+    else
+    {
+      display.print("  ");
+    }
+    display.println("Red");
+
+    if (current_color == 1)
+    {
+      display.print("> ");
+    }
+    else
+    {
+      display.print("  ");
+    }
+    display.println("Green");
+
+    if (current_color == 2)
+    {
+      display.print("> ");
+    }
+    else
+    {
+      display.print("  ");
+    }
+    display.println("Blue");
+  }
+  else
+  {
+    display.setTextSize(2);
+    switch (current_color)
+    {
+    case 0:
+      display.print("Red: ");
+      display.println(rgb[0]);
+      break;
+    case 1:
+      display.print("Green: ");
+      display.println(rgb[1]);
+      break;
+    case 2:
+      display.print("Blue: ");
+      display.println(rgb[2]);
+      break;
+    default:
+      break;
+    }
+  }
+}
+
 void loop()
 {
-
   for (CRGB &pixel : leds)
   {
-    pixel.red++;
-    pixel.blue++;
-    pixel.green++;
+    pixel.red = rgb[0];
+    pixel.blue = rgb[1];
+    pixel.green = rgb[2];
   }
 
   FastLED.show();
+
+  // update display
+  display.clearDisplay();
+  draw_header();
+  draw_body();
+  display.display();
 }
